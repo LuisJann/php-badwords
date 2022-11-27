47c7a5fce4d1ceaa993fe1c7ba6d88b000480c94
LuisJann <giannoneluigi10@gmail.com>
2022-11-27 17:45:27 +0100

    add censuerd function

--- censuredWord.php
+++ censuredWord.php
@@ -1,2 +1,43 @@
 <?php
-echo "ciao";
+$text = $_GET["text"];
+$word = $_GET["word"];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <section>
+        <h2>Testo originale</h2>
+        <p>
+            <?php
+            echo $text;
+            ?>
+        </p>
+        <p>
+            il tuo testo è lungo <?php echo strlen($text) ?> caratteri
+        </p>
+    </section>
+
+    <section>
+        <h2>Testo censurato</h2>
+        <p>
+            <?php
+            $censured_text = str_replace($word, "***", $text);
+            echo $censured_text;
+            ?>
+        </p>
+        <p>
+            il tuo testo è lungo <?php echo strlen($censured_text) ?> caratteri
+        </p>
+    </section>
+</body>
+
+</html>
